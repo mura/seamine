1699cefdc535ddfbf8bd7115f3c88125c198a576
Yohei Murayama <muracchi@gmail.com>
2026-03-20 11:44:45 +0900

    typescriptの設定更新

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname
-});
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...compat.plugins("import"),
   { ignores: ["lib/**/*"] },
   {
     rules: {
